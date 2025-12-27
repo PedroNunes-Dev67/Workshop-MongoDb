f1da841114e909eb225a7f7e5f6b7b0de5ad9371
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2025-12-27 11:15:06 -0300

    Referenciando os posts do usuário

--- src/main/java/PedroNunesDev/WorkshopMongo/config/Instantiation.java
+++ src/main/java/PedroNunesDev/WorkshopMongo/config/Instantiation.java
@@ -34,10 +34,14 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey","bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
-        
+
         Post post1 = new Post(null,LocalDate.parse("21/03/2018", fmt) ,"Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDto(maria));
         Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1,post2));
+
+        userRepository.save(maria);
     }
 }
